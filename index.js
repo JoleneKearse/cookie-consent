ad67a7d72625cbbf5f5ac4e3d8f94d4f0c6e3d83
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-16 16:56:33 -0400

    add modal close button function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
+// set time interval for annoying modal to appear
 setTimeout(function () {
   modal.style.display = "block";
 }, 3000);
+
+// modal close btn function
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
