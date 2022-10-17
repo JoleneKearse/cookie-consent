14846114e0e791cd9d4a3c82127fb6806776e34c
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-17 16:47:56 -0400

    change modal text on submit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const cookies = document.getElementById("cookies");
+const modalText = document.getElementById("modal-text");
 
 // set time interval for annoying modal to appear
 setTimeout(function () {
@@ -12,6 +13,14 @@ modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
+// prevent page from reloading
 cookies.addEventListener("submit", function (e) {
   e.preventDefault();
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+      <img src="images/loading.svg" class="loading">
+      <p id="uploadText">
+        Uploading your data to the dark web...
+      </p>
+  </div>`;
 });
