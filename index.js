5d81274200899629a24eeeaa254c3ab407d88407
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-18 15:44:49 -0400

    minor CSS fixes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ consentForm.addEventListener("submit", function (e) {
   }, 1500);
   setTimeout(function () {
     document.getElementById("modal-inner").innerHTML = `
-      <h2>Thanks you, <span>${name}</span>, AKA sucker! </h2>
+      <h2>Thanks you, <span class="modal-display-name">${name}</span>, AKA sucker! </h2>
       <p>We just sold the rights to your eternal soul.</p>
       <div class="idiot-gif">
           <img src="img/pirate.gif">
